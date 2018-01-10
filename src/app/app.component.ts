8b59eca93d6c3a78fbafe0be12e448b1d3ff468e
EC2 Default User <ec2-user@ip-172-31-77-206.ec2.internal>
2018-01-10 19:00:52 +0000

    firstupload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Notice';
 }
